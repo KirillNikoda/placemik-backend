0694c0c0f273933546667ca2126b1fa79bf11556
KirillNikoda <nikodakirill386@gmail.com>
2021-12-13 23:00:22 +0300

    feat: add db connection, environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
+      cli: {
+        migrationsDir: 'src/migration',
+      },
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
